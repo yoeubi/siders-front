c5bebd3161ac11654f9712fa6e2c9ec283afe11e
JungwonSeong <yoeubi28@gmail.com>
2019-08-21 23:30:07 +0900

    feat:Tab Component ing...

---
+++ components/Tab.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const Tab = () => {
+  return (
+    <div>
+      <Link>
+        <a href="/">로그인/회원가입</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Tab;
